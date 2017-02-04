e35f900acec057f35f6c4b0669936980cc0e6640
ConnorSewell <ctrsewell@dundee.ac.uk>
2017-02-04 23:24:46 +0000

    Sorting video recording into its own class. Still yet to test properly. Delay is temporarily measure taken to ensure video works due to load order.

--- app/src/main/java/com/example/connor/vuzixm100_honoursproject/Main_Activity.java
+++ app/src/main/java/com/example/connor/vuzixm100_honoursproject/Main_Activity.java
@@ -21,6 +21,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 
 import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SurfaceHolder;
@@ -29,26 +30,23 @@ import android.widget.FrameLayout;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class Main_Activity extends Activity
 {
-
-    private CameraPreview cp;
-    private Camera camera;
+    private SurfaceView surfaceView;
+    MediaRecorder mr = new MediaRecorder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_);
-        camera = Camera.open();
-        cp = new CameraPreview(this, camera);
-        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
-        preview.addView(cp);
-        init();
+        surfaceView = (SurfaceView) findViewById(R.id.camera_preview);
+        VideoAudio vd = new VideoAudio(this, surfaceView);
     }
 
     @Override
@@ -72,108 +70,8 @@ public class Main_Activity extends Activity
 
         return super.onOptionsItemSelected(item);
     }
-    MediaRecorder mr = new MediaRecorder();
-    private void init() {
-
-
-
-        camera.unlock();
-        mr.setCamera(camera);
-
-        mr.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
-        mr.setVideoSource(MediaRecorder.VideoSource.CAMERA);
-
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "ACELP");
-
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                System.out.println("Failed to create directory...");
-            }
-        }
-
-        File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                "NewVID_.mp4");
-
-        mr.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
-        mr.setOutputFile(mediaFile.toString());
-
-        System.out.println("Out: " + mediaFile.toString());
-
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                mr.setPreviewDisplay(cp.getHolder().getSurface());
-                try {
-                    mr.prepare();
-                } catch (IllegalStateException e) {
-                    System.out.println("Error: " + String.valueOf(e));
-                } catch (IOException e) {
-                    System.out.println("Error: " + String.valueOf(e));
-                }
-                // this code will be executed after 2 seconds
-                mr.start();
-            }
-        }, 15000);
-
-    }
-
-    /**
-     * https://developer.android.com/guide/topics/media/camera.html#capture-video
-     * ^Used throughout class. Accessed: 25/01/2017 @ 19:10
-     */
-   public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-        private SurfaceHolder mHolder;
-        private Camera mCamera;
-
-        public CameraPreview(Context context, Camera camera) {
-            super(context);
-            mCamera = camera;
-            mHolder = getHolder();
-            mHolder.addCallback(this);
-        }
-
-        public void surfaceCreated(SurfaceHolder holder) {
-            // The Surface has been created, now tell the camera where to draw the preview.
-            try {
-                mCamera.setPreviewDisplay(holder);
-                mCamera.startPreview();
-            } catch (IOException e) {
-                System.out.println("Error with previw: " + String.valueOf(e));
-            }
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // empty. Take care of releasing the Camera preview in your activity.
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-            if (mHolder.getSurface() == null) {
-                // preview surface does not exist
-                return;
-            }
-
-            // stop preview before making changes
-            try {
-                mCamera.stopPreview();
-            } catch (Exception e) {
-                // ignore: tried to stop a non-existent preview
-            }
-
-            // set preview size and make any resize, rotate or
-            // reformatting changes here
-
-            // start preview with new settings
-            try {
-                mCamera.setPreviewDisplay(mHolder);
-                mCamera.startPreview();
-
-            } catch (Exception e) {
-                System.out.println("Error: " + String.valueOf(e));
-            }
-        }
-    }
 }
+
+
+
 
--- app/src/main/java/com/example/connor/vuzixm100_honoursproject/VideoAudio.java
+++ app/src/main/java/com/example/connor/vuzixm100_honoursproject/VideoAudio.java
@@ -14,32 +14,44 @@ import android.widget.FrameLayout;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Connor on 24/01/2017.
  * Handles video/audio data.
  * https://developer.android.com/guide/topics/media/camera.html#capture-video
  * ^ Used throughout class. Accessed: 25/01/2017 @ 19:10
+ * http://stackoverflow.com/questions/1817742/how-can-i-capture-a-video-recording-on-android
+ * ^ Also used to aid with video capturing due to docs code being a bit "poor". Accessed: 04/02/2017 @ 20:27
  */
-public class VideoAudio
+public class VideoAudio implements SurfaceHolder.Callback
 {
     private Context context;
-    private CameraPreview cp;
     private Camera camera;
+    private SurfaceView surfaceView;
+    private SurfaceHolder mHolder;
+    private MediaRecorder mr;
 
-    public VideoAudio(Context context) {
+    public VideoAudio(Context context, SurfaceView surfaceView) {
         this.context = context;
-        cp = new CameraPreview(this.context, camera);
-
+        this.surfaceView = surfaceView;
         camera = Camera.open();
+        mr = new MediaRecorder();
+        mHolder = surfaceView.getHolder();
+        mHolder.addCallback(this);
         init();
     }
 
-    private void init() {
-        MediaRecorder mr = new MediaRecorder();
-
+    public void init()
+    {
         camera.unlock();
         mr.setCamera(camera);
+
+        if (camera == null)
+        {
+            System.out.print("Camera = null");
+        }
 
         mr.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
         mr.setVideoSource(MediaRecorder.VideoSource.CAMERA);
@@ -47,86 +59,72 @@ public class VideoAudio
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "ACELP");
 
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
+        if (!mediaStorageDir.exists())
+        {
+            if (!mediaStorageDir.mkdirs())
+            {
                 System.out.println("Failed to create directory...");
             }
         }
 
         File mediaFile = new File(mediaStorageDir.getPath() + File.separator +
-                "VID_.mp4");
+                "NewVideo.mp4");
 
-        mr.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_HIGH));
+        mr.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_LOW));
         mr.setOutputFile(mediaFile.toString());
 
-        mr.setPreviewDisplay(cp.getHolder().getSurface());
-
-        try
-        {
-            mr.prepare();
-        } catch (IllegalStateException e) {
-            System.out.println("Error: " + String.valueOf(e));
-        } catch (IOException e) {
-            System.out.println("Error: " + String.valueOf(e));
-        }
-
-        //mr.start();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    mr.setPreviewDisplay(mHolder.getSurface());
+                } catch (Exception e) {
+                    System.out.println("Error:" + String.valueOf(e));
+                }
+                try {
+                    mr.prepare();
+                } catch (IllegalStateException e) {
+                    System.out.println("Error: " + String.valueOf(e));
+                } catch (IOException e) {
+                    System.out.println("Error: " + String.valueOf(e));
+                }
+                // this code will be executed after 2 seconds
+                mr.start();
+            }
+        }, 1000);
     }
 
-    /**
-     * https://developer.android.com/guide/topics/media/camera.html#capture-video
-     * ^Used throughout class. Accessed: 25/01/2017 @ 19:10
-     */
-    class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-        private SurfaceHolder mHolder;
-        private Camera mCamera;
+    public void surfaceCreated(SurfaceHolder holder)
+    {
+    }
 
-        public CameraPreview(Context context, Camera camera) {
-            super(context);
-            mCamera = camera;
-            mHolder = getHolder();
-            mHolder.addCallback(this);
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // empty. Take care of releasing the Camera preview in your activity.
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
+        if (mHolder.getSurface() == null) {
+            // preview surface does not exist
+            return;
         }
 
-        public void surfaceCreated(SurfaceHolder holder) {
-            // The Surface has been created, now tell the camera where to draw the preview.
-            try {
-                mCamera.setPreviewDisplay(holder);
-                mCamera.startPreview();
-            } catch (IOException e) {
-                System.out.println("Error with previw: " + String.valueOf(e));
-            }
+        // stop preview before making changes
+        try {
+            camera.stopPreview();
+        } catch (Exception e) {
+            // ignore: tried to stop a non-existent preview
         }
 
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // empty. Take care of releasing the Camera preview in your activity.
-        }
+        try {
+            camera.setPreviewDisplay(mHolder);
+            camera.startPreview();
 
-        public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-            if (mHolder.getSurface() == null) {
-                // preview surface does not exist
-                return;
-            }
-
-            // stop preview before making changes
-            try {
-                mCamera.stopPreview();
-            } catch (Exception e) {
-                // ignore: tried to stop a non-existent preview
-            }
-
-            // set preview size and make any resize, rotate or
-            // reformatting changes here
-
-            // start preview with new settings
-            try {
-                mCamera.setPreviewDisplay(mHolder);
-                mCamera.startPreview();
-
-            } catch (Exception e) {
-                System.out.println("Error: " + String.valueOf(e));
-            }
+        } catch (Exception e) {
+            System.out.println("Error: " + String.valueOf(e));
         }
     }
 }
+
 
